60c2f99296a01201ecaa563179cadc7400f9bba2
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-03-28 20:19:59 -0300

    Find students

--- backend/src/main/java/com/ana/studentsystem/controllers/StudentController.java
+++ backend/src/main/java/com/ana/studentsystem/controllers/StudentController.java
@@ -1,6 +1,9 @@
 package com.ana.studentsystem.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,11 @@ public class StudentController {
 	
 	@Autowired
 	private StudentService service;
+	
+	@GetMapping
+	public List<Student> findAll() {
+		return service.findAll();
+	}
 	
 	@PostMapping
 	public String insert(@RequestBody Student student) {
--- backend/src/main/java/com/ana/studentsystem/services/StudentService.java
+++ backend/src/main/java/com/ana/studentsystem/services/StudentService.java
@@ -1,5 +1,7 @@
 package com.ana.studentsystem.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class StudentService {
 	
 	public Student insert(Student student) {
 		return repository.save(student);
+	}
+	
+	public List<Student> findAll() {
+		return repository.findAll();
 	}
 }
