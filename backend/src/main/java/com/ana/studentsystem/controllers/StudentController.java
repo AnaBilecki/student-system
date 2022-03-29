3deaa6fef1bcb1a38acc2548d6a2788819e734cf
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-03-29 11:01:52 -0300

    Insert student

--- backend/src/main/java/com/ana/studentsystem/controllers/StudentController.java
+++ backend/src/main/java/com/ana/studentsystem/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.ana.studentsystem.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.ana.studentsystem.services.StudentService;
 
 @RestController
 @RequestMapping(value = "/student")
+@CrossOrigin
 public class StudentController {
 	
 	@Autowired
--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import './styles.css';
 
 function Form() {
+
+    const [name, setName] = useState('');
+    const [country, setCountry] = useState('');
+
+    const handleClick = (e: any) => {
+        e.preventDefault()
+        const student = { name, country }
+        console.log(student)
+        fetch("http://localhost:8080/student", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(student)
+
+        }).then(() => {
+            console.log("New student added")
+        })
+    }
+
     return (
         <div className="container">
             <div className="form-container">
@@ -8,14 +27,18 @@ function Form() {
                 <form>
                     <div className="form-group">
                         <label>Student Name</label>
-                        <input type="text" className="form-control" placeholder="Enter student name" />
+                        <input type="text" className="form-control" placeholder="Enter student name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="form-group">
                         <label>Student Country</label>
-                        <input type="text" className="form-control" placeholder="Enter student country" />
+                        <input type="text" className="form-control" placeholder="Enter student country"
+                            value={country}
+                            onChange={(e) => setCountry(e.target.value)} />
                     </div>
                     <div className="submit-button">
-                        <button type="submit" className="btn btn-outline-primary">Submit</button>
+                        <button type="submit" className="btn btn-outline-primary" onClick={handleClick}>Submit</button>
                     </div>
                 </form>
             </div>
