d72c5090749cfe62a8d137165f50a55b3790f010
Ana Carolina Bilecki <anacarolina255@gmail.com>
2022-03-30 12:31:53 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_student(name, country) VALUES ('Maria', 'Portugal');
+INSERT INTO tb_student(name, country) VALUES ('John', 'Canada');
+INSERT INTO tb_student(name, country) VALUES ('Peter', 'England');
+INSERT INTO tb_student(name, country) VALUES ('Ana', 'Brazil');
